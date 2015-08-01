f755967b41208c446cd1a6ee46ee6a36458096bd
Geecatc <ravikumarchaurasia@gmail.com>
2015-08-01 20:44:38 +0530

    initial commit

---
+++ src/com/geecat/main/First.java
@@ -0,0 +1,10 @@
+package com.geecat.main;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Think hard, Try Harder and Never give up.");
+	}
+
+}
